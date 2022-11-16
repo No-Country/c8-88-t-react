920117f98f28d211ba03f417a1846114df7759ab
Gabot3ck <jesus.castilloc@outlook.com>
2022-11-15 21:24:17 -0300

    se agrega la librería hamburguer para el btn hamburguer

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -2,7 +2,7 @@
 
 export const Nav = () => {
     return (<>
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg" >
+        <nav className="navbar navbar-dark bg-primary navbar-expand-lg" >
             <h1>Hola Mundo</h1>
         </nav>
     </>)
