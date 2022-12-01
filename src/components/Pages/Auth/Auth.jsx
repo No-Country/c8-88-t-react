0611768b1b8a6d2076b36413bcd637be634be53e
Gabot3ck <jesus.castilloc@outlook.com>
2022-11-30 22:27:05 -0300

    se hace responsive el auth

--- src/components/Pages/Auth/Auth.jsx
+++ src/components/Pages/Auth/Auth.jsx
@@ -23,21 +23,43 @@ export const Auth = () => {
 
     return (<>
         <div className="w-100" >
-            <div className="wrapper_auth mx-auto  d-flex gap-2">
-                <div className="w-50 d-flex flex-column justify-content-center gap-5 align-items-center">
+            <div className="wrapper_auth mx-auto  d-flex  flex-column flex-lg-row mb-5 mb-xl-0">
+
+                <div className="wrapper_auth_info d-flex flex-column justify-content-center gap-5 align-items-center">
                     <p className="h5 mx-auto text-center m-0">Realiza envíos al exterior mediante viajeros</p>
                     <img  className="mx-auto" src={login} alt="Envía un paquete" />
                 </div>
-                <div className="w-50 d-flex flex-column justify-content-center gap-5 align-items-center">
+
+                <div className="d-flex flex-column justify-content-center gap-5 align-items-center">
                     <span></span>
+
                     <div className="wrapper_btn d-flex flex-column justify-content-between">
-                        <button className="btn-login w-100" onClick={handleFacebook}>Ingresar con Facebook</button>
+                        <button 
+                            className="btn-login w-100 d-flex gap-4 justify-content-start align-items-center ps-5" 
+                            onClick={handleFacebook}>
+                            <i class="bi bi-facebook"></i>
+                            Ingresar con Facebook
+                        </button>
 
-                        <button className="btn-login w-100" onClick={handleGoogle}>Ingresar con Google</button>
+                        <button 
+                            className="btn-login w-100 d-flex gap-4 justify-content-start align-items-center ps-5" 
+                            onClick={handleGoogle}>
+                            <i class="bi bi-google"></i>
+                            Ingresar con Google
+                        </button>
 
-                        <button className="btn-login w-100" onClick={handleEmail}>Ingresar con Email</button>
+                        <button 
+                            className="btn-login w-100 d-flex gap-4 justify-content-start align-items-center ps-5" 
+                            onClick={handleEmail}>
+                            <i class="bi bi-envelope"></i>
+                            Ingresar con Email
+                        </button>
 
-                        <button className="btn-login w-100" onClick={handleLogin}>Login</button>
+                        <button 
+                            className="btn-login w-100" 
+                            onClick={handleLogin}>
+                            Crear Cuenta
+                        </button>
                     </div>
                 </div>
             </div>
