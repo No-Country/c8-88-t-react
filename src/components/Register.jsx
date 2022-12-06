677f98f1e029190f431717b407edd9c2645be85d
Gabot3ck <jesus.castilloc@outlook.com>
2022-12-05 22:53:57 -0300

    se agrega estilos a registro de usuario

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { addUser } from '../reducers/users';
 import { regisEmail } from '../db';
+import "./Register.css";
+import Logo from "../assets/logos/logo alterno.png";
 
 function Register() {
 
@@ -35,97 +37,64 @@ function Register() {
   };
 
 
-  return (
-    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Register to your account
-          </h2>
-        </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div className="mb-2">
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
+  return (<>
+    <div className="wrapper_login d-flex flex-column align-items-center justify-content-center mx-auto py-5">
+      <div className='login_header d-flex flex-column align-items-center justify-content-center'>
+        <img className="" src={Logo} alt="Logo TravelPack"/>
+        <h2 className="h4">Registra tu cuenta</h2>
+      </div>
+
+      <div className='login_main d-flex justify-content-center'>
+        <form className="row g-4 w-100 " onSubmit={handleSubmit}>
+
+        <div className=" col-10  mx-auto d-flex flex-column">
+            <label className="form-label mb-0">Correo electrónico</label>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-                onChange={inputHandle}
-              />
-            </div>
-            <div>
-              <label htmlFor="text" className="sr-only">
-                Full Name
-              </label>
-              <input
-                id="fullName"
-                name="fullName"
-                type="text"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Full Name"
-                onChange={inputHandle}
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
-                onChange={inputHandle}
-              />
-            </div>
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  className="rounded-1 "
+                  placeholder="usuario@correo.com"
+                  onChange={inputHandle}
+                />
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
+          <div className=" col-10  mx-auto d-flex flex-column">
+            <label className="form-label mb-0">Nombre y apellido</label>
               <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
-            </div>
-            <div className="text-sm">
-              <Link
-                to="/forgetpassword"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot your password?
-              </Link>
-            </div>
+                  id="fullName"
+                  name="email"
+                  type="text"
+                  required
+                  className="rounded-1 "
+                  placeholder="Juan Pérez"
+                  onChange={inputHandle}
+                />
           </div>
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Sign in
+
+          <div className=" col-10  mx-auto d-flex flex-column">
+            <label className="form-label mb-0">Contraseña</label>
+              <input
+                  id="pass"
+                  name="password"
+                  type="password"
+                  required
+                  className="rounded-1 "
+                  placeholder="********"
+                  onChange={inputHandle}
+                />
+          </div>
+
+          <div className=" col-10  mx-auto d-flex flex-column mr-auto">
+            <button type="submit" className='btn btn-primary rounded-3 '>
+              Registrar
             </button>
           </div>
         </form>
       </div>
     </div>
-  );
+    </>)
 };
 
 export default Register;
